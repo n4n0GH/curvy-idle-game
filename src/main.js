39c4e22617fac2f0507730ff3decb4aa1fb2695b
n4n0 <n4n0@tuta.io>
2019-08-19 11:26:50 +0200

    gh pages I guess

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueCodeHighlight from 'vue-code-highlight'
-import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import store from './store'
 
@@ -9,7 +8,6 @@ require ('../node_modules/vue-code-highlight/themes/prism.css')
 Vue.config.productionTip = false
 
 Vue.use(VueCodeHighlight)
-Vue.use(BootstrapVue)
 
 new Vue({
   store,
